e6519c213843021eefe38270d51544b01a21f213
calvin <calv2n@berkeley.edu>
2024-08-22 09:37:38 -0700

    Added doc string to optimize function

--- newton.py
+++ newton.py
@@ -5,6 +5,12 @@ def second_derivative(f, x):
     return derivative(f, derivative(f, x))
 
 def optimize(x, f):
+    """Runs Newton's method of optimization for a given function with starting point.
+    
+    Keyword Arguments:
+    x -- Starting position (no default)
+    f -- Function to optimize (no default)
+    """
     x_next = 999
     while x_next - x < 0.1:
         x_next = x - (derivative(f, x) / second_derivative(f, x))
